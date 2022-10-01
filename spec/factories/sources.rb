b5bc1eb4acd82f6907cadbfaeeb2c0ba808e51aa
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-01 16:13:57 +0200

    Covered jumia and carrefour

--- spec/factories/sources.rb
+++ spec/factories/sources.rb
@@ -8,4 +8,14 @@ FactoryBot.define do
     url { 'https://amazon.eg' }
     url_prefix { 'https://amazon.eg' }
   end
+
+  trait :carrefour do
+    url { 'https://www.carrefouregypt.com/' }
+    url_prefix { 'https://www.carrefouregypt.com' }
+  end
+
+  trait :jumia do
+    url { 'https://www.jumia.com.eg/' }
+    url_prefix { 'https://www.jumia.com.eg' }
+  end
 end
--- spec/services/scrapers/product_urls_spec.rb
+++ spec/services/scrapers/product_urls_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe Scrapers::ProductUrls do
       expect(product_urls.empty?).to be_falsey
     end
   end
+
+  context 'when the source is carrefour' do
+    let(:carrefour_source) { create(:source, :carrefour) }
+    let(:carrefour_vegetables_page) { create(:source_page, :carrefour_vegetables_page, source: carrefour_source) }
+    it 'returns product urls' do
+      product_urls = Scrapers::ProductUrls.new(carrefour_vegetables_page.id).perform
+      expect(product_urls).to be_an_instance_of(Array)
+      expect(product_urls.empty?).to be_falsey
+    end
+  end
+
+  context 'when the source is jumia' do
+    let(:jumia_source) { create(:source, :jumia) }
+    let(:jumia_vegetables_page) { create(:source_page, :jumia_vegetables_page, source: jumia_source) }
+    it 'returns product urls' do
+      product_urls = Scrapers::ProductUrls.new(jumia_vegetables_page.id).perform
+      expect(product_urls).to be_an_instance_of(Array)
+      expect(product_urls.empty?).to be_falsey
+    end
+  end
 end
