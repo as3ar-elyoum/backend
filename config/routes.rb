da6a943c58530970f966320f3bd36e0d83cbf5e0
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-10-04 19:05:46 +0200

    Scaffold categories

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
     resources :source_pages
   end
   resources :products
+  resources :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_212801) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_160220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "product_prices", force: :cascade do |t|
     t.bigint "product_id"
