6f0986aa4958b65008aaa82bf12e1c4e12d5d692
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-17 20:26:11 +0200

    Created new logs record

--- app/workers/source_page_worker.rb
+++ app/workers/source_page_worker.rb
@@ -5,6 +5,7 @@ class SourcePageWorker
   def perform(source_page_id, page_number = 0)
     Scrapers::ProductUrls.new(source_page_id, page_number).perform
   rescue StandardError => e
-    SourcePageLog.update(notes: e.message)
+    error_details = "#{e.message} => #{e.backtrace.first}, page_number => #{page_number}"
+    SourcePageLog.create(source_page_id:, notes: error_details)
   end
 end
