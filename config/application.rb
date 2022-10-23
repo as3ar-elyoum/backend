fbc462bd86d368a486d506d562b06776d21d61cf
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-23 23:47:15 +0200

    Fixed event and handlers directory structure

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Dir[File.expand_path('app/events/*.rb')].sort.map do |file|
-  require file
-end
-
-Dir[File.expand_path('app/handlers/*.rb')].sort.map do |file|
-  require file
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -25,6 +17,5 @@ module As3arElyoumBackend
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.autoload_paths << Rails.root.join('app/events/*.rb')
   end
 end
