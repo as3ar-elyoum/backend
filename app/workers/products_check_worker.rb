994bb12fc5138fecaf437e561d758ddf6f4bfc69
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-27 18:00:43 +0200

    Increased the limit

--- app/workers/products_check_worker.rb
+++ app/workers/products_check_worker.rb
@@ -1,7 +1,7 @@
 class ProductsCheckWorker
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: 2
-  MAX_PRODCUTS_COUNT = 100
+  MAX_PRODCUTS_COUNT = 200
   def perform
     Product.active.order(updated_at: :asc)
            .first(MAX_PRODCUTS_COUNT).each(&:enqueue_scraper_worker)
