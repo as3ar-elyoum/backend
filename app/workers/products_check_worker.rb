70f9b9006f9c22b691c3e426af5cd8cc28ad4a8c
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-28 14:41:33 +0200

    Handle Latency Limit

--- app/workers/products_check_worker.rb
+++ app/workers/products_check_worker.rb
@@ -2,10 +2,16 @@ class ProductsCheckWorker
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: 2
   MAX_PRODCUTS_COUNT = 150
+  LATENCY_LIMIT = 60 * 5
   def perform
-    products = Product.order(updated_at: :asc).limit(MAX_PRODCUTS_COUNT)
+    return if exceeds_latency_limit
 
+    products = Product.order(updated_at: :asc).limit(MAX_PRODCUTS_COUNT)
     products.update_all(updated_at: Time.now)
     products.each(&:enqueue_scraper_worker)
+  end
+
+  def exceeds_latency_limit
+    Sidekiq::Queue.new('product_details_scraper').latency / 60 > LATENCY_LIMIT
   end
 end
