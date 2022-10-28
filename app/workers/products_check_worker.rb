e23e9d0561013761039a6503a96b07247c08b59a
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-28 12:05:14 +0200

    Fixed typo

--- app/workers/products_check_worker.rb
+++ app/workers/products_check_worker.rb
@@ -3,10 +3,9 @@ class ProductsCheckWorker
   sidekiq_options queue: :default, retry: 2
   MAX_PRODCUTS_COUNT = 200
   def perform
-    products = Product.order(updated_at: :asc)
-.limit(MAX_PRODCUTS_COUNT)
+    products = Product.order(updated_at: :asc).limit(MAX_PRODCUTS_COUNT)
 
     products.update_all(created_at: Time.now)
-    product.each(&:enqueue_scraper_worker)
+    products.each(&:enqueue_scraper_worker)
   end
 end
