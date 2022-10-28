34f9d2a81df568f234822d4bb70815c6218e195a
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-28 13:00:01 +0200

    Changed the limit

--- app/workers/products_check_worker.rb
+++ app/workers/products_check_worker.rb
@@ -1,7 +1,7 @@
 class ProductsCheckWorker
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: 2
-  MAX_PRODCUTS_COUNT = 200
+  MAX_PRODCUTS_COUNT = 150
   def perform
     products = Product.order(updated_at: :asc).limit(MAX_PRODCUTS_COUNT)
 
