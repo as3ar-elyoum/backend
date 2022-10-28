223112457e6de19e175cb62b9a31bc1f59b53fc6
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-10-28 14:43:11 +0200

    Handle Logic

--- app/workers/products_check_worker.rb
+++ app/workers/products_check_worker.rb
@@ -4,14 +4,15 @@ class ProductsCheckWorker
   MAX_PRODCUTS_COUNT = 150
   LATENCY_LIMIT = 60 * 5
   def perform
-    return if exceeds_latency_limit
+    byebug
+    return if exceeds_latency_limit?
 
     products = Product.order(updated_at: :asc).limit(MAX_PRODCUTS_COUNT)
     products.update_all(updated_at: Time.now)
     products.each(&:enqueue_scraper_worker)
   end
 
-  def exceeds_latency_limit
-    Sidekiq::Queue.new('product_details_scraper').latency / 60 > LATENCY_LIMIT
+  def exceeds_latency_limit?
+    Sidekiq::Queue.new('product_details_scraper').latency > LATENCY_LIMIT
   end
 end
