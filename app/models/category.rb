f718487ef7489d2fe8531fbf575f174274df79b6
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2022-10-30 19:15:20 +0200

    Add initialize cloudinary

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_many :source_pages
-end
+
+  validates :icon, presence: true
+
+  def icon_path
+    icon.attachment.try(:blob).try(:url)
+  end
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+Cloudinary.config do |config|
+  config.cloud_name = ''
+  config.api_key = ''
+  config.api_secret = ''
+  config.secure = true
+  config.cdn_subdomain = true
+end
+
