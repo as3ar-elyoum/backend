67bd67bb0cd43bc04ce18415fb7c9bf0cef26a94
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-11-06 01:27:01 +0200

    Fixed device create

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -3,12 +3,10 @@ module Api
     protect_from_forgery with: :null_session
 
     def create
-      @device = Device.find_or_create_by(device_id: params[:device_id])
+      @device = Device.find_or_create_by(:device_id => params[:device_id])
       @device.fcm_token = params[:fcm_token]
       if @device.save
-        render json: @device
-      else
-        render json: @device.errors, status: :unprocessable_entity
+        render json: @device, status: :created
       end
     end
   end
