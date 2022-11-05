95b99b2fb5c853b19eff289394f66031c09db50b
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-06 00:22:01 +0200

    Renamed column and refactor actions

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -2,16 +2,10 @@ module Api
   class DevicesController < ApplicationController
     protect_from_forgery with: :null_session
 
-    def new
-      @device = Device.new
-    end
-
     def create
-      @device = Device.find_or_create_by(:device_id => params[:device_id])
-      @device.token = params[:token]
-      if @device.save
-        redirect_to root_path
-      end
+      @device = Device.find_or_create_by(device_id: params[:device_id])
+      @device.fcm_token = params[:fcm_token]
+      head :no_content if @device.save
     end
   end
 end
