16498be2c4ba0d7ddc9384b1775ab4d4eea6534b
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-07 19:38:24 +0200

    Remove source filter from backfill service

--- app/services/products/backfill.rb
+++ app/services/products/backfill.rb
@@ -1,7 +1,7 @@
 module Products
   class Backfill
     def self.perform
-      products = Product.where(source_id: 3).order(id: :asc).find_each do |product|
+      products = Product.order(id: :asc).find_each do |product|
         unless product.valid?
           product.unique_identifier = SecureRandom.hex(16)
           product.status = :duplicate
