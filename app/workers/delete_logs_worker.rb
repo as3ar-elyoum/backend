cb7918ab1f0aa6a2620fc1019bf922d59bd18c34
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-11-08 00:16:10 +0200

    Deleted perform parameter

--- app/workers/delete_logs_worker.rb
+++ app/workers/delete_logs_worker.rb
@@ -1,9 +1,9 @@
 class DeleteLogsWorker
     include Sidekiq::Worker
-    sidekiq_options queue: :delete_logs, retry: 1
+    sidekiq_options queue: :default, retry: 1
 
-    def perform(product_id, source_page_id)
-        ProductLog.where(product_id: product_id).where('created_at < ?', 2.days.ago).delete_all
-        SourcePageLog.where(source_page_id: source_page_id).where('created_at < ?', 2.days.ago).delete_all
+    def perform
+        ProductLog.where('created_at < ?', 2.days.ago).delete_all
+        SourcePageLog.where('created_at < ?', 2.days.ago).delete_all
     end
 end
