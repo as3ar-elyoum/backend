d4906f5a468119cfdccd7bcc72639a2e81de9d13
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-13 19:27:01 +0200

    Removed duplicate query

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -14,7 +14,6 @@ module Api
     end
 
     def show
-      @prices = @product.prices.order('created_at DESC').pluck(:created_at, :price)
       @prices = @product.prices.order('created_at DESC').pluck(:created_at, :price).map do |item|
         [item.first.to_date, item.last]
       end
