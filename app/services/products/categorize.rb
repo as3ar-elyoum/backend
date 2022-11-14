7489d590ad05c3fd3a040df3ecf74244554b0e0f
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-15 00:20:13 +0200

    Categorize product once

--- app/services/products/categorize.rb
+++ app/services/products/categorize.rb
@@ -2,8 +2,9 @@ module Products
   class Categorize
     def self.perform(product_id)
       product = Product.find product_id
-      similars = Products::Similar.new(product_id).perform
+      return unless product.category_id.nil?
 
+      similars = Products::Similar.new(product_id).perform
       grouped_by_category = similars.records.group(:category_id).count
       the_common_category = grouped_by_category.invert.max&.last
       product.category_id = the_common_category
