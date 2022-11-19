101f13e9ffdaf36f5ae92f291d9f29d8c486c7b5
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-19 01:34:12 +0100

    Fixed Indexing Data

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,16 +3,23 @@ module Searchable
 
   included do
     include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
+    # include Elasticsearch::Model::Callbacks
 
     def as_indexed_json(_options = {})
-      return {} unless indexable?
-
-      as_json(only: %i[name source_id category_id])
+      as_json(only: %i[name source_id])
     end
 
     def self.search(search_definition)
       __elasticsearch__.search(search_definition)
+    end
+
+    after_commit do
+      __elasticsearch__.index_document if indexable?
+      __elasticsearch__.delete_document if disabled?
+    end
+
+    after_destroy do
+      __elasticsearch__.delete_document if disabled?
     end
   end
 end
