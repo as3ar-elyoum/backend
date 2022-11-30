2a3fead5386713690eccc04566d15e98e3b03856
m7madmagdy <mohamedmagdysaber@gmail.com>
2022-11-30 10:26:15 +0200

    Added app-ads.txt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def main
+    # file_path = "#{Rails.root}/public/files/app-ads.txt"
+
+    # send_data file_path, layout: false, type: 'plain/text'
+  end
+end
