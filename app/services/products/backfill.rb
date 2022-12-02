e26db291ec38c84f8b393b1bf6461a27eb8015da
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-12-03 00:37:33 +0100

    Backfill prices data

--- app/services/products/backfill.rb
+++ app/services/products/backfill.rb
@@ -2,10 +2,9 @@ module Products
   class Backfill
     def self.perform
       products = Product.order(id: :asc).find_each do |product|
-        unless product.valid?
-          product.unique_identifier = SecureRandom.hex(16)
-          product.status = :duplicate
-        end
+        product.price_updated_at = product.prices.last.created_at
+        product.prices_count = product.prices.count
+
         product.save
       rescue StandardError => e
       end
