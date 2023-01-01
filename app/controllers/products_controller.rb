0d5f186d3295fba47bda0335ff039af9a9211976
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-01-01 07:08:23 +0200

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
+      @product.check_similar_category
       redirect_to edit_product_path(@product), notice: 'Product was successfully updated.'
     end
   end
