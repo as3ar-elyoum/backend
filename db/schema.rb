c89f66ef34084eb2c459a54957746c05062f449a
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-02-05 20:47:41 +0200

    Created active flag in categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_180048) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_184430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_180048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "keywords"
+    t.boolean "active"
   end
 
   create_table "devices", force: :cascade do |t|
