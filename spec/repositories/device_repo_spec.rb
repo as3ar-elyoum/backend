cc550836567a2e00181849649231c9a0800b0984
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-02-10 01:26:06 +0200

    increase device repo test

--- spec/repositories/device_repo_spec.rb
+++ spec/repositories/device_repo_spec.rb
@@ -2,9 +2,13 @@ describe DeviceRepo do
     let!(:device) { create :device }
     let!(:repo_instance) { described_class.new }
 
-    context 'updates successfully' do
-        it 'updates device last usage time' do
-            expect { repo_instance.update_usage_time(device.fcm_token) }.to change { device.reload.last_usage_time }
-        end
+    it 'updates device last usage time' do
+        expect { repo_instance.update_usage_time(device.fcm_token) }.to change { device.reload.last_usage_time }
+    end
+
+    it "find_or_create a device by fcm_token" do
+        expect(device.fcm_token).to be_truthy
+        found_or_created_device = Device.find_or_create_by({ fcm_token: device.fcm_token })
+        expect(found_or_created_device).to be_an_instance_of(Device)
     end
 end
