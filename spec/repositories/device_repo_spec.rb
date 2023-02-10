2e93db565f48fc038114e6486bf57bedbb568f70
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-02-10 18:37:17 +0200

    Changed device repo spec test

--- spec/repositories/device_repo_spec.rb
+++ spec/repositories/device_repo_spec.rb
@@ -1,4 +1,4 @@
-describe DeviceRepo do
+RSpec.describe DeviceRepo do
     let!(:device) { create :device }
     let!(:repo_instance) { described_class.new }
 
@@ -6,9 +6,15 @@ describe DeviceRepo do
         expect { repo_instance.update_usage_time(device.fcm_token) }.to change { device.reload.last_usage_time }
     end
 
-    it "find_or_create a device by fcm_token" do
-        expect(device.fcm_token).to be_truthy
-        found_or_created_device = Device.find_or_create_by({ fcm_token: device.fcm_token })
-        expect(found_or_created_device).to be_an_instance_of(Device)
+    context "find_or_create a device by fcm_token" do
+        it "create device if not found" do
+            create_device = Device.create({ fcm_token: device.fcm_token })
+            expect(create_device).to be_an_instance_of(Device)
+        end
+
+        it "find device by fcm_token" do 
+            found_device = Device.find_by ({ fcm_token: device.fcm_token } )
+            expect(found_device).to eq(device)
+        end
     end
 end
