214487e9e46d2c44f2e260f6c895779e5019aeb7
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-02-12 19:29:23 +0100

    Reafctor devices api specs

--- spec/requests/api/post_devices_spec.rb
+++ spec/requests/api/post_devices_spec.rb
@@ -1,24 +1,23 @@
 RSpec.describe Api::DevicesController, type: :controller do
-    let(:test_token) { "test_token" }
+  let(:test_token) { 'test_token' }
 
-    describe 'POST /api/devices/create' do
-        context 'with valid parameters' do
-            it 'creates a new device' do
-                expect { post :create, params: { fcm_token: test_token} }.to change(Device, :count).by(1)
-            end
+  describe 'POST /api/devices/create' do
+    context 'with valid parameters' do
+      it 'creates a new device' do
+        expect { post :create, params: { fcm_token: test_token } }.to change(Device, :count).by(1)
+      end
 
-            it 'returns created device' do
-                post :create, params: { fcm_token: test_token}
-                device = Device.last
-                expect(response.status).to eq 201
-            end
-        end
-        
-        context 'with invalid parameters' do
-            it 'returns Bad Request error' do
-                post :create, params: { fcm_token: nil}
-                expect(response.status).to eq 204
-            end
-        end
-    end 
+      it 'returns created device' do
+        post :create, params: { fcm_token: test_token }
+        expect(response.status).to eq 201
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'returns Bad Request error' do
+        post :create, params: { fcm_token: nil }
+        expect(response.status).to eq 204
+      end
+    end
+  end
 end
