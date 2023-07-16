225339691da1a1c54f453d0388b5bea5d02a6339
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-07-16 14:37:35 +0200

    Fixed urls regex

--- spec/requests/source_pages/create_spec.rb
+++ spec/requests/source_pages/create_spec.rb
@@ -1,28 +1,28 @@
 RSpec.describe 'SourcePagesController', type: :request do
   let(:source) { create(:source) }
-  let(:source_page) { build(:source_page, source: source) }
+  let(:source_page) { build(:source_page, source:) }
 
-    describe '#create' do
-      context '#successful' do
-        it 'creates a source page' do
-         expect{
+  describe '#create' do
+    context '#successful' do
+      it 'creates a source page' do
+        expect do
           source_page_params = source_page.attributes.symbolize_keys
-          post "/sources/#{source_page.source_id}/source_pages", params: { source_page: source_page_params  }
+          post "/sources/#{source_page.source_id}/source_pages", params: { source_page: source_page_params }
           expect(response).to have_http_status(:created)
-         }.to change(SourcePage, :count).by(1)
+        end.to change(SourcePage, :count).by(1)
       end
     end
 
-     context '#failure' do
-        let(:source_page) { build(:source_page, source: source, name: nil) }
-        it 'doesnt create a source page' do
-          expect do
-            source_page_params = source_page.attributes.symbolize_keys
-            post "/sources/#{source_page.source_id}/source_pages", params: { source_page: source_page_params }
-            expect(response).to render_template(:new)
-            expect(response).to have_http_status(:unprocessable_entity)
-          end.not_to change(SourcePage, :count)
-        end
-     end
+    context '#failure' do
+      let(:source_page) { build(:source_page, source:, name: nil) }
+      it 'doesnt create a source page' do
+        expect do
+          source_page_params = source_page.attributes.symbolize_keys
+          post "/sources/#{source_page.source_id}/source_pages", params: { source_page: source_page_params }
+          expect(response).to render_template(:new)
+          expect(response).to have_http_status(:unprocessable_entity)
+        end.not_to change(SourcePage, :count)
+      end
+    end
   end
 end
