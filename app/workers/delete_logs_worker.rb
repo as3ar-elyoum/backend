c919672445cf99b52d30c2f5db46d168e5560057
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-08-05 16:52:48 +0200

    Removed delete worker

--- app/workers/delete_logs_worker.rb
+++
@@ -1,8 +0,0 @@
-class DeleteLogsWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :default, retry: 1
-
-  def perform
-    ProductLog.where('created_at < ?', 2.days.ago).delete_all
-  end
-end
